c8a771d1cfec9cc2c3e5135c0b74b9a471dc9053
Your Name <you@example.com>
2022-12-08 08:39:53 -0600

    "this is all just practice stuff. to delete from github later"

--- Angular-lessons/angular_practice/angular-lesson/src/app/app.component.ts
+++ Angular-lessons/angular_practice/angular-lesson/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-lesson';
+  title = 'My First Angular Application';
+  name = "Tumaini Ussiri";
+  itemList: string[] = ['My first website', 'Web Development Project', 'Test with code Pen'];
+  rectangle = {
+    length: 5,
+    width:6,
+    area: function () {
+      return this.length * this.width;
+    }
+  }
 }
